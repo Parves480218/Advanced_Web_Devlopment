1a6a35225fb6e12bcf77860afdad8905763f53e2
Parves480218 <parves.imran@centria.fi>
2026-01-29 19:38:26 +0200

    all video notes

---
+++ index.js
@@ -0,0 +1,52 @@
+// const place = 'World';
+// const greeting = "Hello";
+// //console.log("%s,%s",greeting,place);
+
+// console.log(`${greeting},${place}`)
+// 
+// const place = 'World';
+// const greeting = "Hello";
+// 
+// console.log(`${greeting},${place}`)
+
+        //  define different variablwes using var , let and const
+// var hello = 'Hello';
+
+
+// console.log(hello)
+
+// hello = 'Hello javaScript';
+// console.log(hello)
+
+// if(true){
+//     let peak = "Horizon"
+//     console.log(peak)
+// }
+
+// const pier = "Long";
+// console.log(pier)
+// // pier = "Short";
+// // console.log(pier)
+         // Working with strings
+         //  demo with string methods with + operator and spacing
+
+
+// let str1 = "Hello";
+// let str2 = 'World!';
+
+// console.log(str1+str2);
+// console.log(str1 +  " Big " + str2);
+
+// let num1 = 1;
+// let num2 = '1';
+
+// console.log(num1+num2);
+// console.log(num1+1);
+
+   //concatenation with template literals
+// let str1 = 'javascript';
+// let str2 = 'rocks';
+
+// console.log(`I am writing code in ${str1}`);
+// console.log(`Formatting in  ${str1} is ${str2}!`)
+
